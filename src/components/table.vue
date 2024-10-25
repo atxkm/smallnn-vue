a6f24af438e0f7d32a7b1d2a14a22628b7f1ca17
汤晓奎 <tangxiaokui@cestc.cn>
2024-10-25 22:32:00 +0800

    feat: 按钮状态

--- src/components/table.vue
+++ src/components/table.vue
@@ -31,7 +31,7 @@
     <el-table-column label="操作" width="260">
       <template #default="scope">
         <el-button
-          v-if="scope.row.phone"
+          v-if="scope.row.phone && scope.row.progress <= 0"
           type="primary"
           @click="startScan(scope)"
         >
